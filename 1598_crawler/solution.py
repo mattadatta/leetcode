9b56888d1f3c0679095edee714be86ac569091dd
Matthew Brown <me.matt.brown@gmail.com>
2024-07-10 01:15:23 -0400

    1598

---
+++ 1598_crawler/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        count = 0
+        for log in logs:
+            if log == "./":
+                continue
+            else:
+                if log == "../":
+                    if count > 0:
+                        count -= 1
+                else:
+                    count += 1
+        return count
+
+
+sol = Solution()
+logs = ["d1/","d2/","../","d21/","./"]
+result = sol.minOperations(logs)
+print(result)
